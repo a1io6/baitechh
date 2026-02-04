0315d47d38a4f3c0e0ef47104c44cf68db64e400
a1io6 <bilihobs@icloud.com>
2026-02-04 11:53:44 +0600

    master

--- src/app/(auth)/codeverify/page.jsx
+++ src/app/(auth)/codeverify/page.jsx
@@ -1,7 +1,7 @@
 import { CodeVerifyContent } from '@/components/auth/code/CodeVerify'
 import React from 'react'
 
-function page() {
+function codeverify() {
   return (
     <>
       <CodeVerifyContent type="verify"/>
@@ -9,4 +9,4 @@ function page() {
   )
 }
 
-export default page
+export default codeverify
